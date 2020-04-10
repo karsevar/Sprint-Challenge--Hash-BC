d8c261575059a30f15da0b170f843b6343274c97
masonkarsevar <masonkarsevar@gmail.com>
2020-04-10 08:30:00 -0700

    ex2.py finished and passed all tests. WIP: blockchain

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,32 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
-    pass
+    # loop through the tickets array:
+        # insert each element into hashtable with 
+        # source as the key and destination as the value 
+
+    # initialize current key variable with NONE
+
+    # loop through the tickets array again with a for loop 
+    # with a duration of length 
+        # insert key value within route array through route[index] = current_key 
+        # find the next key in the hashtable 
+    
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    current_key = 'NONE'
+
+    for index in range(length):
+        current_key = hash_table_retrieve(hashtable, current_key)
+        route[index] = current_key
+
+    return route[:-1]
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+result = reconstruct_trip(tickets, 3)
+print(result)
